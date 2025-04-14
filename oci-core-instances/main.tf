f03a19da7db709994d4187c150721956b6818db6
Raul Diaz Palomo <raul.diaz.palomo@oracle.com>
2025-04-14 15:38:05 +0200

    fixing

--- oci-core-instances/main.tf
+++ oci-core-instances/main.tf
@@ -1,7 +1,8 @@
 resource "oci_core_instance" "this" {
   for_each = var.instance_params
-
-  availability_domain  = data.oci_identity_availability_domains.ads.availability_domains[each.value.ad - 1].name
+  
+  availability_domain  = "ad1"
+  # availability_domain  = data.oci_identity_availability_domains.ads.availability_domains[each.value.ad - 1].name
   compartment_id       = var.compartment_ocid
   shape                = each.value.shape
   display_name         = each.value.hostname
@@ -13,6 +14,15 @@ resource "oci_core_instance" "this" {
       ocpus         = each.value.instance_shape_config_ocpus
       baseline_ocpu_utilization = each.value.baseline_ocpu_utilization
     }
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = var.images[each.value.os].image
+  }
+  
+  create_vnic_details {
+    subnet_id = each.value.subnet_name
   }
 
   metadata = {

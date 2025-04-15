7b811e5610efea14e8933c43e5da78f8048a1eec
Raul Diaz Palomo <raul.diaz.palomo@oracle.com>
2025-04-15 10:57:58 +0200

    edit variables resource manager

--- oci-resource-manager/variables.tf
+++ oci-resource-manager/variables.tf
@@ -1,22 +1,10 @@
 variable "stack_params" {
   description = "Map of stack configurations for each client"
   type = map(object({
-    stack_name = string
-    folder     = string
+    stack_name           = string
+    folder               = string
+    compartment_id       = string
+    source_provider_id   = string
+    repo_url             = string
   }))
-}
-
-variable "compartment_id" {
-  type        = string
-  description = "OCI Compartment OCID"
-}
-
-variable "source_provider_id" {
-  type        = string
-  description = "OCI Source Provider OCID"
-}
-
-variable "repo_url" {
-  type        = string
-  description = "Git repository URL for stack source"
 }
